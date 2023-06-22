f3051ea34350b0b8fe5c616bc815cd2e6c2e1657
phuocbui <phuocbui@kms-technology.com>
2023-06-22 11:33:33 +0700

    backup

---
+++ react_router/src/App.tsx
@@ -0,0 +1,21 @@
+import MainLayout from 'layouts/MainLayout'
+import About from 'pages/About'
+import Dashboard from 'pages/Dashboard'
+import StaffList from 'pages/StaffList'
+
+function App() {
+  return (
+    <div className='App'>
+      <MainLayout>
+        <Route>
+          
+        </Route>
+        <Dashboard />
+        <About />
+        <StaffList />
+      </MainLayout>
+    </div>
+  )
+}
+
+export default App
